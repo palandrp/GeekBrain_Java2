1e9db24a9732520f3e3057b7ab8cff8ad1e5f1bd
palandr <palandrp@gmail.com>
2017-06-16 23:37:31 +0500

    Тест массива

--- Collections/src/ru/kimdo/Main.java
+++ Collections/src/ru/kimdo/Main.java
@@ -1,5 +1,11 @@
 package ru.kimdo;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.List;
+
 /**
  * 1) Создать массив с набором слов (20-30 слов, должны встречаться повторяющиеся):
  * а. Посчитать сколько раз встречается каждое слово;
@@ -14,6 +20,30 @@ package ru.kimdo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        new arrayFromText("Java 2. Методичка #3.docx");
     }
+}
+
+class arrayFromText {
+    List<String> textList;
+
+    arrayFromText(String FILE_NAME){
+        String line;
+        String file = "";
+        String[] textArray;
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(
+                        new FileInputStream(FILE_NAME)))){
+            while ((line = reader.readLine()) != null)
+                file += line + "\n";
+        } catch (Exception e){
+            System.out.println(e);
+        }
+        if (file.length() > 0) {
+            textArray = file.split(" |,|.|!|-|<|>|:|;|}|[|]");
+            //textList = new LinkedList<String>(textArray);
+            System.out.println(textArray.toString());
+        }
+    }
+    
 }

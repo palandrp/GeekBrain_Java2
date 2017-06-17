e15eb68484c2b2b44853ca502a414baa563c9ff8
palandr <palandrp@gmail.com>
2017-06-17 16:45:17 +0500

    Готов GUI

--- NetChat/src/ru/kimdo/Main.java
+++ NetChat/src/ru/kimdo/Main.java
@@ -11,56 +11,41 @@ import java.awt.event.ActionListener;
  */
 
 public class Main extends JFrame implements ActionListener {
-
-    final String TITLE_OF_PROGRAM = "Network chat";
-    final int START_LOCATION = 200;
-    final int WINDOW_WIDTH = 350;
-    final int WINDOW_HEIGHT = 450;
-
-    JTextArea dialogue;
-    JCheckBox ai;
-    JTextField message;
+    private JTextArea dialogue;
+    private JTextField message;
 
     public static void main(String[] args) {
         new Main();
     }
+    private Main() {
+        final String TITLE_OF_PROGRAM = "Network chat";
+        final int START_LOCATION = 200;
+        final int WINDOW_WIDTH = 350;
+        final int WINDOW_HEIGHT = 450;
 
-    Main() {
         setTitle(TITLE_OF_PROGRAM);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
-
         dialogue = new JTextArea();
         dialogue.setEditable(true);
         JScrollPane scrollBar = new JScrollPane(dialogue);
-
         JPanel bp = new JPanel();
         bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
-        ai = new JCheckBox("IA");
-        ai.doClick();
         message = new JTextField();
         message.addActionListener(this);
         JButton enter = new JButton("Enter");
         enter.addActionListener(this);
-
-        bp.add(ai);
         bp.add(message);
         bp.add(enter);
         add(BorderLayout.CENTER, scrollBar);
         add(BorderLayout.SOUTH, bp);
-
         setVisible(true);
-
-        sbot = new SimpleBot();
-
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
         if (message.getText().trim().length() > 0) {
             dialogue.append(message.getText() + "\n");
-            dialogue.append(TITLE_OF_PROGRAM.substring(0, 9) +
-                    sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+            dialogue.append("Тестовый запуск!\n");
         }
         message.setText("");
         message.requestFocusInWindow();

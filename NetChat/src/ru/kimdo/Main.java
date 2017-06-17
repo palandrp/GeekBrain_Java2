c606e556239e5081c51131949be8ed64d2b715a7
palandr <palandrp@gmail.com>
2017-06-17 16:36:23 +0500

    Базовый GUI, взятый из ChatBot'a.

--- NetChat/src/ru/kimdo/Main.java
+++ NetChat/src/ru/kimdo/Main.java
@@ -1,8 +1,68 @@
 package ru.kimdo;
 
-public class Main {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * @author Pavel Petrikovskiy
+ * @version 17.06.17
+ */
+
+public class Main extends JFrame implements ActionListener {
+
+    final String TITLE_OF_PROGRAM = "Network chat";
+    final int START_LOCATION = 200;
+    final int WINDOW_WIDTH = 350;
+    final int WINDOW_HEIGHT = 450;
+
+    JTextArea dialogue;
+    JCheckBox ai;
+    JTextField message;
 
     public static void main(String[] args) {
-	// write your code here
+        new Main();
+    }
+
+    Main() {
+        setTitle(TITLE_OF_PROGRAM);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setBounds(START_LOCATION, START_LOCATION, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+        dialogue = new JTextArea();
+        dialogue.setEditable(true);
+        JScrollPane scrollBar = new JScrollPane(dialogue);
+
+        JPanel bp = new JPanel();
+        bp.setLayout(new BoxLayout(bp, BoxLayout.X_AXIS));
+        ai = new JCheckBox("IA");
+        ai.doClick();
+        message = new JTextField();
+        message.addActionListener(this);
+        JButton enter = new JButton("Enter");
+        enter.addActionListener(this);
+
+        bp.add(ai);
+        bp.add(message);
+        bp.add(enter);
+        add(BorderLayout.CENTER, scrollBar);
+        add(BorderLayout.SOUTH, bp);
+
+        setVisible(true);
+
+        sbot = new SimpleBot();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (message.getText().trim().length() > 0) {
+            dialogue.append(message.getText() + "\n");
+            dialogue.append(TITLE_OF_PROGRAM.substring(0, 9) +
+                    sbot.sayInReturn(message.getText(), ai.isSelected()) + "\n");
+        }
+        message.setText("");
+        message.requestFocusInWindow();
     }
 }

174ed10c43332861b210f3e2e319dcc3a0e13866
palandr <palandrp@gmail.com>
2017-06-24 13:54:53 +0500

    Теперь основной поток ждет завершение дочернего

--- Threads/src/ru/kimdo/Main.java
+++ Threads/src/ru/kimdo/Main.java
@@ -35,12 +35,17 @@ class MyClass {
         System.arraycopy(arr,0,a1,0,h);
         System.arraycopy(arr,h,a2,0,h);
         TestThreadClass forTread = new TestThreadClass(a1);
-        Thread tread_1 = new Thread(forTread);
-        tread_1.start();
+        Thread thread_1 = new Thread(forTread);
+        thread_1.start();
         for (int i = 0; i < h; i++){
             a2[i] = (float)(a2[i]   * Math.sin(0.2f + i/5)
                                     * Math.cos(0.2f + i/5)
                                     * Math.cos(0.4f + i/2));
+        }
+        try {
+            thread_1.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.arraycopy(forTread.getA(),0,arr,0,h);
         System.arraycopy(a2,0,arr,h,h);

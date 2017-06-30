768162e1d44a8069637e17b49dd97cee2ec818d0
palandr <palandrp@gmail.com>
2017-06-30 13:16:03 +0500

    Добавил классы сервер, клиент-хендлер и клиент. Пока что без взаимосвязи с GIU и мэйном.

--- NetChat/src/ru/kimdo/Main.java
+++ NetChat/src/ru/kimdo/Main.java
@@ -6,9 +6,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowEvent;
 import java.awt.event.WindowListener;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Scanner;
 
 /**
  * @author Pavel Petrikovskiy
@@ -81,5 +82,146 @@ public class Main extends JFrame implements ActionListener {
         }
         message.setText("");
         message.requestFocusInWindow();
+    }
+}
+class MyLittleServer implements Runnable {
+    public void run(){
+        ServerSocket server = null;
+        Socket s = null;
+        Thread client = null;
+
+        try {
+            server = new ServerSocket(8189);
+            System.out.println("Сервер запущен. Ожидание клиентов...");
+            s = server.accept();
+            System.out.println("Клиент подключился");
+            client = new Thread(new ClientHandler(s));
+            client.start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                client.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            try {
+                server.close();
+                System.out.println("Server closed");
+                s.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+class ClientHandler implements Runnable {
+    private Socket s;
+    private PrintWriter out;
+    private BufferedReader in;
+    private Scanner s_input;
+    private static int CLIENTS_COUNT = 0;
+    private String name;
+
+    ClientHandler(Socket s) {
+        s_input = new Scanner(System.in);
+
+        try {
+            this.s = s;
+            out = new PrintWriter(s.getOutputStream());
+            in = new BufferedReader(
+                    new InputStreamReader(s.getInputStream()));
+            CLIENTS_COUNT++;
+            name = "Клиент #" + CLIENTS_COUNT;
+            System.out.println("Клиент хендлер \"" + name + "\" готов к приёму");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public void run() {
+        Thread output = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message = "";
+                try {
+                    while (true) {
+                        message = name + ": " + in.readLine();
+                        System.out.println(message);
+                        if (message.equalsIgnoreCase(name + ": END")) {
+                            out.println("Server: end session");
+                            out.flush();
+                            break;
+                        }
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }); output.start();
+        while (output.isAlive()) {
+            out.println("Server: " + s_input.nextLine());
+            out.flush();
+        }
+        try {
+            System.out.println("Клиент отключился");
+            s.close();
+            s_input.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+class MyLittleClient {
+    private Socket sock;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    MyLittleClient() {
+        final String SERVER_ADDR = "localhost";
+        final int SERVER_PORT = 8189;
+        Scanner c_input = new Scanner(System.in);
+
+        try {
+            sock = new Socket(SERVER_ADDR, SERVER_PORT);
+            in = new BufferedReader(
+                    new InputStreamReader(sock.getInputStream()));
+            out = new PrintWriter(sock.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Thread output = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                String message = "";
+                try {
+                    while (true) {
+                        message = in.readLine();
+                        System.out.println(message);
+                        if (message.equalsIgnoreCase("Server: end session"))
+                            break;
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }); output.start();
+        try {
+            while (output.isAlive()) {
+                out.println(c_input.nextLine());
+                out.flush();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            out.println("end");
+            out.flush();
+            sock.close();
+            out.close();
+            in.close();
+            c_input.close();
+        } catch (IOException exc) {
+            exc.printStackTrace();
+        }
     }
 }
